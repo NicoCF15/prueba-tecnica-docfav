11cf8121f9e840c1b98a0bbc26f4be4591dc3bbb
Nicolás Cofré <ncofrefaces@gmail.com>
2025-02-27 14:18:58 -0300

    fix: change error codes

--- src/domain/Exceptions/InvalidEmailException.php
+++ src/domain/Exceptions/InvalidEmailException.php
@@ -5,7 +5,7 @@ namespace App\Domain\Exceptions;
 class InvalidEmailException extends \InvalidArgumentException
 {
     // Se puede agregar más lógica si se requiere
-    public function __construct(string $message = "El formato del correo electrónico es inválido", int $code = 0, \Exception $previous = null)
+    public function __construct(string $message = "El formato del correo electrónico es inválido", int $code = 400, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
--- src/domain/Exceptions/InvalidNameException.php
+++ src/domain/Exceptions/InvalidNameException.php
@@ -5,7 +5,7 @@ namespace App\Domain\Exceptions;
 class InvalidNameException extends \InvalidArgumentException
 {
     // Se puede agregar más lógica si se requiere
-    public function __construct(string $message = "El nombre es invalido", int $code = 0, \Exception $previous = null)
+    public function __construct(string $message = "El nombre es invalido", int $code = 400, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
