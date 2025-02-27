af27eed0fe805993d6f26dc4f267ff4f11629caf
Nicolás Cofré <ncofrefaces@gmail.com>
2025-02-27 20:37:09 -0300

    fix: add status code to user already exists exception

--- src/domain/Exceptions/UserAlreadyExistsException.php
+++ src/domain/Exceptions/UserAlreadyExistsException.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Domain\Exception;
+namespace App\Domain\Exceptions;
 
 class UserAlreadyExistsException extends \InvalidArgumentException
 {
     // Se puede agregar más lógica si se requiere
-    public function __construct(string $message = "El usuario ya existge en la base de datos", int $code = 0, \Exception $previous = null)
+    public function __construct(string $message = "El usuario ya existe en la base de datos", int $code = 400, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
